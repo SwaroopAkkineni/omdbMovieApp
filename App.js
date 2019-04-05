b58f739c3e538d8552cfaa29346dec1d231bc73d
Swaroop Akkineni <swa7@pitt.edu>
2019-04-05 15:31:56 -0400

    can display basic information

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Episode from './src/Episode';
 import Season from './src/Season';
 import TitleScreen from './src/TitleScreen';
 import TvShowInfo from './src/TvShowInfo';
@@ -6,6 +7,7 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const AppNavigator = createStackNavigator(
   {
+    Episode: Episode,
     Season: Season,
     TitleScreen: TitleScreen,
     TvShowInfo: TvShowInfo,
--- src/Episode.js
+++ src/Episode.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
-import {FlatList, Platform, StyleSheet, Text, View} from 'react-native';
-import Episode from './Episode';
+import {FlatList, Image, Platform, StyleSheet, Text, View} from 'react-native';
 
-export default class Season extends React.Component {
+export default class Episode extends React.Component {
   state = {
-    episodes: [],
-	};
+    episode: '',
+  };
 
   componentDidMount() {
-    this.getEpisodes();
+    this.getEpisode();
   }
 
-  getEpisodes = () => {
+
+  getEpisode = () => {
     const { navigation } = this.props;
+    const episode = navigation.getParam('episode');
     const showName = navigation.getParam('showName');
     const season = navigation.getParam('season');
     let fetchString = 'http://www.omdbapi.com/?t=';
@@ -20,14 +21,14 @@ export default class Season extends React.Component {
     showName.split(' ').forEach(function(str) {
       fetchString += str + '+'
     });
-    fetchString += '&season=' + season + '&apikey=92e3aa84'
+    fetchString += '&season=' + season + '&episode=' + episode +'&apikey=92e3aa84'
     console.log('fetchString: ', fetchString);
     fetch(fetchString)
       .then((response) => response.json())
       .then((responseJson) => {
         console.log('responseJson: ', responseJson);
         this.setState({
-          episodes: responseJson.Episodes,
+          episode: responseJson,
         });
       })
       .catch((error) => {
@@ -35,22 +36,17 @@ export default class Season extends React.Component {
       });
   }
 
+
   render() {
-    const { episodes } = this.state;
-    const { navigation } = this.props;
-    const showName = navigation.getParam('showName');
-    const season = navigation.getParam('season');
+    const {episode} = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{showName}</Text>
-        <Text style={styles.instructions}>Season {season} </Text>
-        <FlatList
-          data={episodes}
-          renderItem={({ item }) => (
-            <Text style={styles.instructions}
-                  onPress={() => navigation.navigate('Episode', { episode: item.Episode, season: season, showName: showName})}>Episode: {item.Episode} {item.Title} </Text>
-          )}
-        />
+        <Text style={styles.welcome}>{episode.Title}</Text>
+        <Text style={styles.instructions}>Director {episode.Director} </Text>
+        <Text style={styles.instructions}>written by: {episode.Writer} </Text>
+        <Text style={styles.instructions}>Rating: {episode.Writer} </Text>
+        <Text style={styles.instructions}>Plot: {episode.Plot} </Text>
+        <Image style={{width: 100, height: 100}} source={{uri: episode.Poster}}/>
       </View>
     );
   }
@@ -61,7 +57,6 @@ export default class Season extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
